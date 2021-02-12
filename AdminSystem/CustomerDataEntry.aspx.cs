543d2a00d0015f68e5e49c8f033ad17547259b91
Ayaanhsnn <P2517318@my365.dmu.ac.uk>
2021-02-12 17:38:50 +0000

    Created properties for customer table design and added a basic presentation layer for customer management

--- AdminSystem/CustomerDataEntry.aspx.cs
+++ AdminSystem/CustomerDataEntry.aspx.cs
@@ -4,11 +4,29 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1_DataEntry : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
 
+    }
+
+    protected void btnOK_Click(object sender, EventArgs e)
+    {
+        //create a new instance of clsCustomer
+        clsCustomer AnCustomer = new clsCustomer();
+        //Capture the customer Id
+        AnCustomer.CustomerId = Convert.ToInt32(txtCustomerId.Text);
+        AnCustomer.Username = txtUsername.Text;
+        AnCustomer.Password = txtPassword.Text;
+        AnCustomer.Address = txtAddress.Text;
+        AnCustomer.DateOfBirth = Convert.ToDateTime(txtDateOfBirth.Text);
+        AnCustomer.ReceiveMail = chkReceiveMail.Checked;
+        //store the address in the session object
+        Session["AnCustomer"] = AnCustomer;
+        //Navigate to the viewer page
+        Response.Redirect("CustomerViewer.aspx");
     }
 }
