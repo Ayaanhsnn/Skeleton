b204e340445084d88e6c90440eab3d54f67c9d18
Wafa <wafaabbasi008@gmail.com>
2021-02-13 01:42:13 +0000

    data entry and viewer

--- AdminSystem/StockViewer.aspx.cs
+++ AdminSystem/StockViewer.aspx.cs
@@ -4,11 +4,19 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1Viewer : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        clsStock AStock = new clsStock();
+        AStock = (clsStock)Session["AStock"];
+        Response.Write(AStock.StockNo + "\t");
+        Response.Write(AStock.OrderNo + "\t");
+        Response.Write(AStock.StockDesc + "\t");
+        Response.Write(AStock.DatePurch + "\t");
+        Response.Write(AStock.Availability + "\t");
+        Response.Write(AStock.Quantity + "\t");
     }
 }
