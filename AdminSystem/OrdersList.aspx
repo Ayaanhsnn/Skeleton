bcd976353ac47515401dfdede06c248ad4f4ee03
Humaira Abdulgafar <P2517104@my365.dmu.ac.uk>
2021-03-15 10:15:36 +0000

    collection for presentation layer

--- AdminSystem/OrdersList.aspx
+++ AdminSystem/OrdersList.aspx
@@ -10,6 +10,7 @@
     <form id="form1" runat="server">
         <div>
         </div>
+        <asp:ListBox ID="lstOrderList" runat="server" Height="686px" Width="914px"></asp:ListBox>
     </form>
 </body>
 </html>
---
+++ ClassLibrary/clsOrderCollection.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace ClassLibrary
+{
+    public class clsOrderCollection
+    {
+        List<ClsOrder> mOrderList = new List<ClsOrder>();
+        public List<ClsOrder> OrderList
+        {
+            get
+            {
+                return mOrderList;
+            }
+            set
+            {
+                mOrderList = value;
+            }
+        }
+        public int Count
+        {
+            get
+            {
+                return mOrderList.Count;
+            }
+            set
+            {
+
+            }
+        }
+       
+        public ClsOrder ThisOrder { get; set; }
+        public clsOrderCollection()
+        { }
+    }
+}
